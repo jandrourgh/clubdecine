081bda051b60635feedebcfe74acac343dd25472
jandrourgh <adiaziborra@gmail.com>
2024-06-26 17:59:19 +0200

    fix(): allow barbie and improve search

--- src/components/MovieData.tsx
+++ src/components/MovieData.tsx
@@ -12,6 +12,7 @@ import {
   FormControl,
   Form,
   ModalFooter,
+  Image,
 } from "react-bootstrap";
 const MovieData = ({
   data,
@@ -35,32 +36,50 @@ const MovieData = ({
   return (
     <Row>
       {data.map((movie, i) => (
-        <Col lg={3} md={4} sm={6} xs={12} key={i} className="p-3">
-          <Card
-            role="button"
+        <Col
+          lg={3}
+          md={4}
+          sm={6}
+          xs={12}
+          key={i}
+          className="my-2"
+          style={{
+            backgroundImage: `url(${baseURL}${movie.backdrop_path})`,
+            backgroundSize: "contain",
+          }}
+        >
+          <Row
+            xs={12}
             onClick={(evt) =>
               handlePropose(movie.id as number, movie.title as string)
             }
-            className="bg-black text-white rounded-0"
+            style={{ backdropFilter: `blur(10px) brightness(50%)` }}
+            className="text-white rounded-0"
           >
-            <Card.Header className="text-center">
-              <h5>{movie.title}</h5>
+            <Col className="text-center p-2">
+              <p>
+                <b>{movie.original_title}</b> <br />{" "}
+                <i className="tw-light">{movie.title}</i>
+              </p>
               <span className="fw-light">
                 <em>{`${movie.release_date?.slice(0, 4)}`}</em>
               </span>
-            </Card.Header>
-            {movie.poster_path ? (
-              <Card.Img
-                className="rounded-0"
-                src={`${baseURL}${movie.poster_path}`}
-              ></Card.Img>
-            ) : (
-              <div className="p-3 text-center">
-                {" "}
-                No hay imagen disponible :(
-              </div>
-            )}
-          </Card>
+            </Col>
+            <Col xs={6} className="d-flex p-0">
+              {movie.poster_path ? (
+                <Image
+                  style={{ width: "100%" }}
+                  className="rounded-0"
+                  src={`${baseURL}${movie.poster_path}`}
+                ></Image>
+              ) : (
+                <div className="p-3 text-center">
+                  {" "}
+                  No hay imagen disponible :(
+                </div>
+              )}
+            </Col>
+          </Row>
         </Col>
       ))}
       <Modal show={modal} onHide={() => setModal(false)}>
@@ -93,7 +112,9 @@ const MovieData = ({
                 disabled={reason === ""}
                 variant="light"
               >
-                Proponer {name}
+                {Boolean(name.toLowerCase().match(/barbie/gm))
+                  ? `Quiero seguir torturando a mis amigues`
+                  : `Proponer ${name}`}
               </Button>
             </Col>
           </Row>
--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -3,15 +3,15 @@ import { useState, useEffect } from "react";
 
 const useFetch = (url: string, shouldFetch: boolean) => {
   const [data, setData] = useState<MovieResult[]>([]);
-  const queryURL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_THEMOVIEDB}&query=${url}`
+  const queryURL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_THEMOVIEDB}&query=${url}&language=es`;
 
   useEffect(() => {
-    if(shouldFetch){
+    if (shouldFetch) {
       fetch(queryURL)
         .then((res) => res.json())
         .then((data: MovieResultsResponse) => {
-          if(data.results){
-            setData(data.results)
+          if (data.results) {
+            setData(data.results);
           }
         });
     }
